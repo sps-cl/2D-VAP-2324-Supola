403aa3ddc8025303bf1a9316c9e3ef393af90b7e
Šimon Šupola <77693994+Narthast@users.noreply.github.com>
2024-04-23 16:30:28 +0200

    Konzolové aplikace - 3. samostatná práce

---
+++ Konzolové aplikace - třetí samostatná práce/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+class ConsoleApp
+{
+    private List<string> words = new List<string>();
+    private int currentIndex = -1;
+
+    void AddWord(string word)
+    {
+        words.Add(word);
+        currentIndex = words.Count - 1;
+        Console.WriteLine(word);
+    }
+
+    void GoBack()
+    {
+        if (currentIndex > 0)
+        {
+            currentIndex--;
+        }
+        Console.WriteLine(words[currentIndex]);
+    }
+
+    void GoForward()
+    {
+        if (currentIndex < words.Count - 1)
+        {
+            currentIndex++;
+        }
+        Console.WriteLine(words[currentIndex]);
+    }
+
+    void Run()
+    {
+        while (true)
+        {
+            Console.Write("Vstup: ");
+            string command = Console.ReadLine();
+
+            if (command.StartsWith("Pridat:"))
+            {
+                string word = command.Substring(7);
+                AddWord(word);
+            }
+            else if (command == "Zpet")
+            {
+                GoBack();
+            }
+            else if (command == "Vpred")
+            {
+                GoForward();
+            }
+            else
+            {
+                Console.WriteLine("Neznámý příkaz.");
+            }
+        }
+    }
+
+    static void Main()
+    {
+        ConsoleApp app = new ConsoleApp();
+        app.Run();
+    }
+}
+
